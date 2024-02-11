4a6d9019738e8451b6bf64012c3cba76979ce33c
HeidkeRed <cebg_slp@hotmail.com>
2024-02-11 17:04:40 -0600

    conexion con la base de datos

---
+++ config/conexion.php
@@ -0,0 +1,16 @@
+<?php
+require_once('global.php');
+
+// Crear conexión
+$conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+} else {
+    echo "Conexión exitosa a la base de datos.";
+}
+
+// Cerrar conexión
+$conn->close();
+?>
---
+++ config/global.php
@@ -0,0 +1,21 @@
+<?php
+
+// Definir la constante para la dirección IP del servidor de base de datos
+define("DB_HOST", "127.0.0.1:33065");
+
+// Definir la constante para el nombre de la base de datos
+define("DB_NAME", "xd");
+
+// Definir la constante para el nombre de usuario de la base de datos
+define("DB_USERNAME", "root");
+
+// Definir la constante para la contraseña del usuario de la base de datos
+define("DB_PASSWORD", "123456789");
+
+// Definir la constante para la codificación de caracteres
+define("DB_ENCODE", "utf8");
+
+// Definir la constante para el nombre del proyecto
+define("PRO_NOMBRE", "proyecto");
+
+?>

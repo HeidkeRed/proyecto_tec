db9ab1f49e0e20d0d520fd4dae00ff00c3d7850e
HeidkeRed <cebg_slp@hotmail.com>
2024-02-12 19:19:47 -0600

    plantilla y funciones para la conexion

--- config/conexion.php
+++ config/conexion.php
@@ -11,7 +11,37 @@ if ($conn->connect_error) {
     echo "Conexión exitosa a la base de datos.";
 }
 
+if (!function_exists('ejecutarConsulta')) {
+    function ejecutarConsulta($sql)
+    {
+        global $conn;
+        $query = $conn->query($sql);
+        return $query;
+    }
+
+    function ejecutarConsultaSimpleFila($sql)
+    {
+        global $conn;
+        $query = $conn->query($sql);
+        $row = $query->fetch_assoc();
+        return $row;
+    }
+
+    function ejecutarConsulta_retornarID($sql) // Aquí estaba el error
+    {
+        global $conn;
+        $query = $conn->query($sql);
+        return $conn->insert_id;
+    }
+
+    function limpiarCadena($str)
+    {
+        global $conn;
+        $str = mysqli_real_escape_string($conn, trim($str));
+        return htmlspecialchars($str);
+    }
+}
+
 // Cerrar conexión
 $conn->close();
 ?>
-
